6613072227c9728b9f918158c3b68c9daf4bd6d6
QHTrung <qhtrung23072000@gmail.com>
2023-09-13 13:19:28 +0700

    random 8 pairs of colors

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,25 @@
+function shufle(colorList) {
+  if (!Array.isArray(colorList) || colorList.length < 2) return colorList
+  for (let i = colorList.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * i)
+    let temp = colorList[i]
+    colorList[i] = colorList[j]
+    colorList[j] = temp
+  }
+}
 export const getRandomColorPairs = (count) => {
   // receive count --> return count * 2 random colors
   // using lib: https://github.com/davidmerfield/randomColor
+  const colorList = []
+  const hueList = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'monochrome']
+  for (let i = 0; i < count; i++) {
+    const color = window.randomColor({
+      luminosity: 'dark',
+      hue: hueList[i % hueList.length],
+    })
+    colorList.push(color)
+  }
+  const fullColorList = [...colorList, ...colorList]
+  shufle(fullColorList)
+  return fullColorList
 }
